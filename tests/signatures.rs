b8d46c97b947058dc4534b47f320ff8d03ebb1db
WingZer0o <mikemulchrone987@gmail.com>
2025-01-04 07:27:29 -0500

    #16 adding more ed25519 tests

--- tests/signatures.rs
+++ tests/signatures.rs
@@ -1,6 +1,6 @@
 #[cfg(test)]
 mod ed25519 {
-    use cas_lib::signatures::ed25519::{ed25519_sign_with_key_pair, ed25519_sign_with_key_pair_threadpool, ed25519_verify_with_public_key, ed25519_verify_with_public_key_threadpool, get_ed25519_key_pair, get_ed25519_key_pair_threadpool};
+    use cas_lib::signatures::ed25519::{ed25519_sign_with_key_pair, ed25519_sign_with_key_pair_threadpool, ed25519_verify_with_key_pair, ed25519_verify_with_key_pair_threadpool, ed25519_verify_with_public_key, ed25519_verify_with_public_key_threadpool, get_ed25519_key_pair, get_ed25519_key_pair_threadpool};
 
     #[test]
     pub fn get_key_pair_test() {
@@ -33,7 +33,7 @@ mod ed25519 {
     }
 
     #[test]
-    pub fn verifiy_with_key_pair() {
+    pub fn verifiy_with_public_key_() {
         let key_pair = get_ed25519_key_pair();
         let message_to_sign = b"Hello World Message To Sign";
         let signature = ed25519_sign_with_key_pair(key_pair, message_to_sign);
@@ -42,11 +42,29 @@ mod ed25519 {
     }
 
     #[test]
-    pub fn verifiy_with_key_pair_threadpool() {
+    pub fn verifiy_with_public_key_threadpool() {
         let key_pair = get_ed25519_key_pair_threadpool();
         let message_to_sign = b"Hello World Message To Sign";
         let signature = ed25519_sign_with_key_pair_threadpool(key_pair, message_to_sign);
         let verification = ed25519_verify_with_public_key_threadpool(signature.public_key, signature.signature, message_to_sign);
+        assert_eq!(verification, true);
+    }
+
+    #[test]
+    pub fn verifiy_with_key_pair() {
+        let key_pair = get_ed25519_key_pair();
+        let message_to_sign = b"Hello World Message To Sign";
+        let signature = ed25519_sign_with_key_pair(key_pair, message_to_sign);
+        let verification = ed25519_verify_with_key_pair(key_pair, signature.signature, message_to_sign);
+        assert_eq!(verification, true);
+    }
+
+    #[test]
+    pub fn verifiy_with_key_pair_threadpool() {
+        let key_pair = get_ed25519_key_pair();
+        let message_to_sign = b"Hello World Message To Sign";
+        let signature = ed25519_sign_with_key_pair_threadpool(key_pair, message_to_sign);
+        let verification = ed25519_verify_with_key_pair_threadpool(key_pair, signature.signature, message_to_sign);
         assert_eq!(verification, true);
     }
 }
