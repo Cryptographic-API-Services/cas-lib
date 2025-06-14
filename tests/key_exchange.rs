983b23b3d3d3be86f37978085d9e353282c8f010
Mike Mulchrone <mikemulchrone987@gmail.com>
2025-06-13 23:56:01 -0400

    key exchange tests

---
+++ tests/key_exchange.rs
@@ -0,0 +1,24 @@
+#[cfg(test)]
+mod key_exchange {
+    use cas_lib::key_exchange::{cas_key_exchange::CASKeyExchange, x25519::{X25519SecretPublicKeyResult, X25519}};
+
+    #[test]
+    pub fn x25519_diffie_hellman() {
+        let alice: X25519SecretPublicKeyResult = X25519::generate_secret_and_public_key();
+        let bob: X25519SecretPublicKeyResult = X25519::generate_secret_and_public_key();
+
+        let alice_shared_secret = X25519::diffie_hellman(alice.secret_key.clone(), bob.public_key.clone());
+        let bob_shared_secret = X25519::diffie_hellman(bob.secret_key.clone(), alice.public_key.clone());
+        assert_eq!(alice_shared_secret, bob_shared_secret);
+    }
+
+    #[test]
+    pub fn x25519_diffie_hellman_threadpool() {
+        let alice: X25519SecretPublicKeyResult = X25519::generate_secret_and_public_key_threadpool();
+        let bob: X25519SecretPublicKeyResult = X25519::generate_secret_and_public_key_threadpool();
+
+        let alice_shared_secret = X25519::diffie_hellman_threadpool(alice.secret_key.clone(), bob.public_key.clone());
+        let bob_shared_secret = X25519::diffie_hellman_threadpool(bob.secret_key.clone(), alice.public_key.clone());
+        assert_eq!(alice_shared_secret, bob_shared_secret);
+    }
+}
