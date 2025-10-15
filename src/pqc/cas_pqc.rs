62642d2993384d3b5628d2f4a1210d14a59ab283
WingZer0o <mikemulchrone987@gmail.com>
2025-10-14 23:36:40 -0400

    changes to types so they are FFI safe

--- src/pqc/cas_pqc.rs
+++ src/pqc/cas_pqc.rs
@@ -1,20 +1,24 @@
+#[repr(C)]
 #[derive(Debug, Clone)]
 pub struct MlKemKeyPair {
     pub secret_key: Vec<u8>,
     pub public_key: Vec<u8>,
 }
 
+#[repr(C)]
 #[derive(Debug, Clone)]
 pub struct MlKemEncapResult {
     pub ciphertext: Vec<u8>,
     pub shared_secret: Vec<u8>,
 }
 
+#[repr(C)]
 #[derive(Debug, Clone)]
 pub struct MlKemSharedSecret {
     pub shared_secret: Vec<u8>,
 }
 
+#[repr(C)]
 #[derive(Debug, Clone)]
 pub struct SlhDsaKeyPair {
     pub signing_key: Vec<u8>,
