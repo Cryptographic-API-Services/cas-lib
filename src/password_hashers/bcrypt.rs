83e35fdb61dbd0aab0ac677f95bc9ae557ae6b24
Mike Mulchrone <mikemulchrone987@gmail.com>
2025-10-29 22:38:37 -0400

    bcrypt parameters

--- src/password_hashers/bcrypt.rs
+++ src/password_hashers/bcrypt.rs
@@ -1,17 +1,23 @@
-
-
 use bcrypt::{hash, verify, DEFAULT_COST};
 
-use super::cas_password_hasher::CASPasswordHasher;
 
 pub struct CASBCrypt;
 
-impl CASPasswordHasher for CASBCrypt {
+impl CASBCrypt {
     /// Hashes a password using bcrypt.
     /// Returns the hashed password as a string.
     fn hash_password(password_to_hash: String) -> String {
         return hash(password_to_hash, DEFAULT_COST).unwrap();
     }
+
+    /// Hashes a password using bcrypt with a specified strength (cost).
+    /// Strength must be between 4 and 31.
+    fn hash_password_with_strength(password_to_hash: String, strength: u32) -> String {
+        if (strength < 4) || (strength > 31) {
+            panic!("Bcrypt strength must be between 4 and 31");
+        }
+        return hash(password_to_hash, strength).unwrap();
+    }   
 
     /// Verifies a password against a hashed password using bcrypt.
     /// Returns true if the password matches the hashed password, false otherwise.
