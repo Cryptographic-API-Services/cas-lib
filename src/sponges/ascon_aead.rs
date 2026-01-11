8245ff1e642032774e8242f326647800894b2831
Mike Mulchrone <mikemulchrone987@gmail.com>
2026-01-11 10:54:33 -0500

    updating ascon

--- src/sponges/ascon_aead.rs
+++ src/sponges/ascon_aead.rs
@@ -1,21 +1,20 @@
-
-
-
-use aes_gcm::AeadCore;
-use ascon_aead::{aead::{generic_array::GenericArray, Aead, KeyInit, OsRng}, AsconAead128 as Ascon128};
+use ascon_aead::{AsconAead128 as Ascon128, Key, AsconAead128Nonce, aead::{Aead, KeyInit}};
+use rand::{rngs::OsRng, RngCore};
 
 use super::cas_ascon_aead::{CASAsconAead};
 pub struct AsconAead;
 
-impl CASAsconAead for AsconAead {
+impl CASAsconAead for AsconAead {   
     /// Encrypts with AscondAead
     fn encrypt(key: Vec<u8>, nonce: Vec<u8>, plaintext: Vec<u8>) -> Vec<u8> {
         if key.len() != 16 || nonce.len() != 16 {
             panic!("Key and nonce must be 16 bytes long");
         }
-        let key_generic_array = GenericArray::from_slice(&key);
-        let nonce_generic_array = GenericArray::from_slice(&nonce);
-        let cipher = Ascon128::new(key_generic_array);
+        let key_array: [u8; 16] = key.try_into().unwrap();
+        let key_generic_array = Key::<Ascon128>::from(key_array);
+        let nonce_array: [u8; 16] = nonce.try_into().unwrap();
+        let nonce_generic_array = AsconAead128Nonce::from(nonce_array);
+        let cipher = Ascon128::new(&key_generic_array);
         let ciphertext = cipher.encrypt(&nonce_generic_array, plaintext.as_ref()).unwrap();
         ciphertext
     }
@@ -25,20 +24,26 @@ impl CASAsconAead for AsconAead {
         if key.len() != 16 || nonce.len() != 16 {
             panic!("Key and nonce must be 16 bytes long");
         }
-        let key_generic_array = GenericArray::from_slice(&key);
-        let nonce_generic_array = GenericArray::from_slice(&nonce);
-        let cipher = Ascon128::new(key_generic_array);
+        let key_array: [u8; 16] = key.try_into().unwrap();
+        let key_generic_array = Key::<Ascon128>::from(key_array);
+        let nonce_array: [u8; 16] = nonce.try_into().unwrap();
+        let nonce_generic_array = AsconAead128Nonce::from(nonce_array);
+        let cipher = Ascon128::new(&key_generic_array);
         let plaintext = cipher.decrypt(&nonce_generic_array, ciphertext.as_ref()).unwrap();
         plaintext
     }
     
     /// Generates a 16-byte key for Ascon Aead
     fn generate_key() -> Vec<u8> {
-        return Ascon128::generate_key(&mut OsRng).to_vec();
+        let mut key_bytes = [0u8; 16];
+        OsRng.fill_bytes(&mut key_bytes);
+        return Key::<Ascon128>::from(key_bytes).to_vec();
     }
     
     /// Generates a Ascon Aead nonce
     fn generate_nonce() -> Vec<u8> {
-        return Ascon128::generate_nonce(&mut OsRng).to_vec();
+        let mut nonce_bytes = [0u8; 16];
+        OsRng.fill_bytes(&mut nonce_bytes);
+        return AsconAead128Nonce::from(nonce_bytes).to_vec();
     }
 }
